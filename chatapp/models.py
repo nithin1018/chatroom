01d9b6fba96461e0aa48293ed3cd7ecab2ba9475
Nithin Jayakumar <nithinjayakumar2205@gmail.com>
2025-07-26 09:35:08 +0530

    alter the profile id to have blank value

--- chatapp/models.py
+++ chatapp/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     bio = models.TextField(max_length=200, blank=True)
-    profile_id = models.CharField(unique=True)
+    profile_id = models.CharField(null=True,blank=True)
 
     def __str__(self):
         return f"{self.profile_id} -- {self.user.username}"
